76c3c789156d7aaccd6e258fa2074c25e097ec7a
lf7817 <535536456@qq.com>
2023-05-30 17:19:22 +0800

    chore: 部署

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import routes from './routes';
 
-export const router = createBrowserRouter(routes);
+export const RouterBasename =
+  import.meta.env.MODE === 'development' ? '' : import.meta.env.BASE_URL;
+
+export const router = createBrowserRouter(routes, { basename: RouterBasename });
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,22 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-      // 兼容 css-loader 加载path alias格式 https://github.com/webpack-contrib/css-loader#url
-      "~@": path.resolve(__dirname, "src"),
-      images: path.resolve(__dirname, "src/assets/images"),
-      "~images": path.resolve(__dirname, "src/assets/images"),
-      styles: path.resolve(__dirname, "src/assets/styles"),
-      "~styles": path.resolve(__dirname, "src/assets/styles"),
+export default defineConfig((options) => {
+  return {
+    base: options.mode === 'development' ? '/' : '/react-three-demo',
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        // 兼容 css-loader 加载path alias格式 https://github.com/webpack-contrib/css-loader#url
+        '~@': path.resolve(__dirname, 'src'),
+        images: path.resolve(__dirname, 'src/assets/images'),
+        '~images': path.resolve(__dirname, 'src/assets/images'),
+        styles: path.resolve(__dirname, 'src/assets/styles'),
+        '~styles': path.resolve(__dirname, 'src/assets/styles'),
+      },
     },
-  },
+  };
 });
